81d092aa93b218634941d06ad11bce0daab11e4b
canalescanorff15-ux <canalescanorff15@gmail.com>
2026-09-13 13:39:40 -0300

    V6.75: ativa acabamento visual do catálogo

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './premium.css';
 import './home-v672.css';
 import './home-v673.css';
 import './catalog-v674.css';
+import './catalog-v675.css';
 import PremiumExperience from '@/components/PremiumExperience';
 import { QuoteListProvider } from '@/components/QuoteListProvider';
 import { CompareProvider } from '@/components/CompareProvider';
