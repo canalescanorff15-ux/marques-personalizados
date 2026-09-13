df6d9a73bcaf14d4b51cc2c9b92341fa42aca318
canalescanorff15-ux <canalescanorff15@gmail.com>
2026-09-13 17:34:36 -0300

    feat: render JSON-LD with request nonce

---
+++ components/JsonLd.tsx
@@ -0,0 +1,7 @@
+import { headers } from 'next/headers';
+import { jsonLd } from '@/lib/seo';
+
+export default async function JsonLd({data}:{data:unknown}){
+  const nonce=(await headers()).get('x-nonce')||undefined;
+  return <script nonce={nonce} type="application/ld+json" dangerouslySetInnerHTML={{__html:jsonLd(data)}}/>;
+}
