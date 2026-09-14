bfbcc392cc8aa5f97ac3ddf4a43d344c8528cd9d
canalescanorff15-ux <canalescanorff15@gmail.com>
2026-09-14 20:42:37 -0300

    style: carregar camada visual V6.87

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import './premium.css';
 import './shell-v681.css';
 import './catalog-v676.css';
+import './inspiration-v687.css';
 import PremiumExperience from '@/components/PremiumExperience';
 import { QuoteListProvider } from '@/components/QuoteListProvider';
 import { CompareProvider } from '@/components/CompareProvider';
