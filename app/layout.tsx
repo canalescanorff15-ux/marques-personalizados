4c93e5ab2a3fe2dc9ffb14a50b3575f7a79f53a5
canalescanorff15-ux <canalescanorff15@gmail.com>
2026-09-20 15:33:17 -0300

    V8.01: ativar camada de curadoria visual

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ import './public-v718.css';
 import './public-v719.css';
 import './public-v721.css';
 import './v8-design-system.css';
+import './v8-image-curation.css';
 import './v8-image-policy.css';
 import PremiumExperience from '@/components/PremiumExperience';
 import { QuoteListProvider } from '@/components/QuoteListProvider';
