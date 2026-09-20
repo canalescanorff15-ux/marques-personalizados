c10dc9adfc6d11c7aed967f1513f9253b1ad6c82
canalescanorff15-ux <canalescanorff15@gmail.com>
2026-09-19 23:50:42 -0300

    V7.19: carregar camada final premium

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ import './public-v715.css';
 import './public-v716.css';
 import './public-v717.css';
 import './public-v718.css';
+import './public-v719.css';
 import PremiumExperience from '@/components/PremiumExperience';
 import { QuoteListProvider } from '@/components/QuoteListProvider';
 import { CompareProvider } from '@/components/CompareProvider';
